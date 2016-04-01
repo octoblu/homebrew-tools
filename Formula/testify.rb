b2b31a86bc8898f5d9e4e16ed1758ca059374835
sqrt <sqrt@octoblu.com>
2016-04-01 12:12:51 -0700

    Use vars for template files

--- Formula/testify.rb
+++ Formula/testify.rb
@@ -1,12 +1,14 @@
 class Testify < Formula
   desc "Setup travis and mocha to run tests"
   homepage "https://github.com/octoblu/node-dev-tools"
-  url "https://github.com/octoblu/node-dev-tools/archive/v1.0.1.tar.gz"
-  sha256 "781893dbc49f41e898a43dcb2a0099ada1d2d609f3f230ab2173ba24b0bada9b"
+  url "https://github.com/octoblu/node-dev-tools/archive/v1.0.2.tar.gz"
+  sha256 "a760b794d24bd6eee33389b15142ccc7a8a1f8f8c46c48c028208625a5e3f5b9"
 
   depends_on "octoblu/tools/travisify"
 
   def install
+    inreplace "bash/testify", 'TEMPLATES_DIR="$(get_templates_dir)"', "TEMPLATES_DIR=\"#{var}/testify\""
     bin.install "bash/testify"
+    var.install "templates/testify"
   end
 end
--- Formula/travisify.rb
+++ Formula/travisify.rb
@@ -1,10 +1,12 @@
 class Travisify < Formula
   desc "Create a travis yaml and enable travis"
   homepage "https://github.com/octoblu/node-dev-tools"
-  url "https://github.com/octoblu/node-dev-tools/archive/v1.0.1.tar.gz"
-  sha256 "781893dbc49f41e898a43dcb2a0099ada1d2d609f3f230ab2173ba24b0bada9b"
+  url "https://github.com/octoblu/node-dev-tools/archive/v1.0.2.tar.gz"
+  sha256 "a760b794d24bd6eee33389b15142ccc7a8a1f8f8c46c48c028208625a5e3f5b9"
 
   def install
+    inreplace "bash/travisify", 'TEMPLATES_DIR="$(get_templates_dir)"', "TEMPLATES_DIR=\"#{var}/travisify\""
     bin.install "bash/travisify"
+    var.install "templates/travisify"
   end
 end
