15e312ca7859cc2518ca66ba968ad3af50738aa3
sqrt <sqrt@octoblu.com>
2016-05-02 13:41:26 -0700

    Update vulcansync to 2.0.0

--- Formula/vulcansync.rb
+++ Formula/vulcansync.rb
@@ -1,8 +1,8 @@
 class Vulcansync < Formula
   desc "Sync vulcand frontends, backends, and middlewares to the major cluster"
   homepage "https://github.com/octoblu/ops-tools"
-  url "https://github.com/octoblu/ops-tools/archive/v1.1.1.tar.gz"
-  sha256 "426be929fd82fb57a098a15924cb850c7baef2d98335233912edd7e838bb1b8a"
+  url "https://github.com/octoblu/ops-tools/archive/vulcansync-v2.0.0.tar.gz"
+  sha256 "47819e22109d16ba0712872ca8d0cb4619f130a6f0330863b0707fc5c8ef0dd0"
 
   depends_on 'octoblu/tools/vctl'
 
