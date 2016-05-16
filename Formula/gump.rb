ae65a035d335a64cacf96292d40295296279a855
Sqrt of Saturn <sqrt@octoblu.com>
2016-05-16 09:04:14 -0700

    updated gump

--- Formula/gump.rb
+++ Formula/gump.rb
@@ -1,9 +1,8 @@
 class Gump < Formula
   desc "Unix dev tools created by Octoblu"
-  url "https://github.com/octoblu/unix-dev-tools/archive/v1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "7e89874780b08502346158c5db13c91072ff0e3d10c4c7f77cba290cfca4429f"
-
+  url "https://github.com/octoblu/unix-dev-tools/archive/v1.1.1.tar.gz"
+  version "1.1.1"
+  sha256 "6db8e78bd0285a22718fb13a8e19bcc564c549be1dfe0d6e888abb97d4688891"
   depends_on 'jq'
   depends_on 'coreutils'
   depends_on 'octoblu/tools/semver'
