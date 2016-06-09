48193920e8b1c23feb302cd4880f6b405e68890f
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-06-09 11:34:25 -0700

    update gump

--- Formula/gump.rb
+++ Formula/gump.rb
@@ -1,8 +1,7 @@
 class Gump < Formula
   desc "Unix dev tools created by Octoblu"
-  url "https://github.com/octoblu/unix-dev-tools/archive/v1.1.1.tar.gz"
-  version "1.1.1"
-  sha256 "6db8e78bd0285a22718fb13a8e19bcc564c549be1dfe0d6e888abb97d4688891"
+  url "https://github.com/octoblu/unix-dev-tools/archive/v1.2.0.tar.gz"
+  sha256 "86adb2d2ec4274d1e1ba8758382e3c6556dc636f396a3ad86edecb1d8d92d20c"
   depends_on 'jq'
   depends_on 'coreutils'
   depends_on 'octoblu/tools/semver'
@@ -12,6 +11,6 @@ class Gump < Formula
   end
 
   test do
-    assert_equal "v1.1.0", shell_output("#{bin}/gump --version").strip
+    assert_equal "v1.2.0", shell_output("#{bin}/gump --version").strip
   end
 end
