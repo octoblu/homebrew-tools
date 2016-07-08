901b8907ff96c44e921be68e0b1106049f82eafe
sqrt <sqrt@octoblu.com>
2016-07-08 09:29:20 -0700

    Update gump to v1.3.0

--- Formula/gump.rb
+++ Formula/gump.rb
@@ -1,16 +1,22 @@
 class Gump < Formula
   desc "Unix dev tools created by Octoblu"
-  url "https://github.com/octoblu/unix-dev-tools/archive/v1.2.0.tar.gz"
-  sha256 "86adb2d2ec4274d1e1ba8758382e3c6556dc636f396a3ad86edecb1d8d92d20c"
+  url "https://github.com/octoblu/unix-dev-tools-gump/archive/v1.3.0.tar.gz"
+  sha256 "695055d3ca7fc294ed5c1d4d034b8f122937920c3e2a228f2714489d4e15bbff"
   depends_on 'jq'
   depends_on 'coreutils'
   depends_on 'octoblu/tools/semver'
 
   def install
-    bin.install "bash/gump"
+    inreplace "gump.sh", 'local directory="$(script_directory)"', ""
+    inreplace "gump.sh", 'local version=$(cat "$directory/VERSION")', "local version=$(cat #{var}/VERSION)"
+
+    move "gump.sh", "gump"
+
+    bin.install "gump"
+    var.install "VERSION"
   end
 
   test do
-    assert_equal "v1.2.0", shell_output("#{bin}/gump --version").strip
+    assert_equal "v1.3.0", shell_output("#{bin}/gump --version").strip
   end
 end
