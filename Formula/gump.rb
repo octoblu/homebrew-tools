e33e05e178ce8477e120e3d01064546c235ac126
sqrt <sqrt@octoblu.com>
2016-07-12 14:15:13 -0700

    Update gump to v1.4.1

--- Formula/gump.rb
+++ Formula/gump.rb
@@ -1,9 +1,9 @@
 class Gump < Formula
-  VERSION = "1.4.0"
+  VERSION = "1.4.1"
 
   desc "Unix dev tools created by Octoblu"
   url "https://github.com/octoblu/unix-dev-tools-gump/archive/v#{VERSION}.tar.gz"
-  sha256 "49c6c917696b6417aaac60ea6f939da4f8860622684ae6cefbb29a0b9b3aaf8d"
+  sha256 "3e16f71a031437a0be9d5bb95259a0ce109ae693f801f83f88bc9546ce20fb84"
   depends_on 'jq'
   depends_on 'coreutils'
   depends_on 'octoblu/tools/semver'
