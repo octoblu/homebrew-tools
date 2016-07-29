13f007286d9c5bf5ef66c2b236089084075b7b95
Peter DeMartini <thepeterdemartini@gmail.com>
2016-07-29 12:53:45 -0700

    updated fireup to 1.1.2

--- Formula/fireup.rb
+++ Formula/fireup.rb
@@ -1,9 +1,9 @@
 class Fireup < Formula
-  VERSION = "1.1.1"
+  VERSION = "1.1.2"
 
   desc "Fireup a project"
   url "https://github.com/octoblu/unix-dev-tools-fireup/archive/v#{VERSION}.tar.gz"
-  sha256 "7d7dc4667896fdd44237cce82cc9cb70ea3deb6d4964791cf9a8f1c3f90f8fee"
+  sha256 "ab0234e76eb5ce3680b53d1b95ada7da3c0e48f14c42bc09933ed3f8f3dc6bfd"
   depends_on 'hub'
 
   def install
