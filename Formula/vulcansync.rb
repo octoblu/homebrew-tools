c7e920dbd21ffe94ecb14db5103b715e01c2df4c
sqrt <sqrt@octoblu.com>
2016-08-01 14:40:31 -0700

    Use vulcansync 4.0.0

--- Formula/vulcansync.rb
+++ Formula/vulcansync.rb
@@ -1,10 +1,10 @@
 class Vulcansync < Formula
-  VERSION = "3.0.0"
+  VERSION = "4.0.0"
 
   desc "Sync vulcand frontends, backends, and middlewares to the major cluster"
   homepage "https://github.com/octoblu/ops-tools-vulcansync"
   url "https://github.com/octoblu/ops-tools-vulcansync/archive/v#{VERSION}.tar.gz"
-  sha256 "592dd96241ff8cfe841f001714f467e14e9519413b80e0a0c0894c4ab51704f8"
+  sha256 "cce37962b71d6e1b5529d22de3bd231c8664c72d45226c308be6c025d79e1585"
 
   depends_on 'octoblu/tools/vctl'
 
