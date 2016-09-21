5e1f391f1318fab3fb830682a1693d5b0635f7f7
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-09-20 17:00:55 -0700

    update testify

--- Formula/testify.rb
+++ Formula/testify.rb
@@ -1,8 +1,8 @@
 class Testify < Formula
   desc "Setup travis and mocha to run tests"
-  url "https://github.com/octoblu/node-dev-tools/archive/v3.3.3.tar.gz"
-  version "3.3.3"
-  sha256 "2e3a7e9b95710e8d4aec840f95b07f93f5b6ef9b4209a56b66c52ed2fd7af78f"
+  url "https://github.com/octoblu/node-dev-tools/archive/v3.4.0.tar.gz"
+  version "3.4.0"
+  sha256 "4fc02ea9d49f17d22b47e5c3c0e9fbb4d98ae9be9dcaf2a3f279288dfd8d4bb1"
 
   depends_on "octoblu/tools/travisify"
 
