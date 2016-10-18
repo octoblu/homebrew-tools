78c8cc8215c61eccc7f6a99f8a1d9f72e55a36e7
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-10-18 09:39:47 -0700

    update gump

--- Formula/gump.rb
+++ Formula/gump.rb
@@ -1,9 +1,9 @@
 class Gump < Formula
-  VERSION = "3.1.0"
+  VERSION = "3.1.1"
 
   desc "Unix dev tools created by Octoblu"
   url "https://github.com/octoblu/unix-dev-tools-gump/archive/v#{VERSION}.tar.gz"
-  sha256 "46606aac21b002776a4ea46f9e7769f910ff10d59b3df29f4ffc1f72998bb6c5"
+  sha256 "e501baef75d09532528e8f4d7c8556ade63a8cc2329d15eaf0f0ae307c8ba8fc"
   depends_on 'jq'
   depends_on 'hub'
   depends_on 'curl'
