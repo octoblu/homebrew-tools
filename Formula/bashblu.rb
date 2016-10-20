5512ae3db5c95377aa0129acfe6962b68017aafb
Peter DeMartini <thepeterdemartini@gmail.com>
2016-10-20 07:16:54 -0700

    updated bashblu to 3.0.0

--- Formula/bashblu.rb
+++ Formula/bashblu.rb
@@ -1,13 +1,13 @@
 class Bashblu < Formula
-  VERSION = "2.0.2"
+  VERSION = "3.0.0"
 
   desc "Generate bash scripts"
   url "https://github.com/octoblu/unix-dev-tools-bashblu/archive/v#{VERSION}.tar.gz"
-  sha256 "7a55510f87c2049a6608a88f8977bbf98e978fa15c6384299bfda95ea8b9ad48"
+  sha256 "a29e4e17a28e3aa3cc9984c93fc2dfc91d1b7e69426910b964c6f412173dafd4"
 
   def install
     inreplace "bashblu.sh", 'local directory="$(script_directory)"', ""
-    inreplace "bashblu.sh", 'echo "unknown"', "echo '#{VERSION}'"
+    inreplace "bashblu.sh", 'echo "unknown-version"', "echo '#{VERSION}'"
 
     move "bashblu.sh", "bashblu"
 
