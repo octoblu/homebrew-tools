068fbfd8b40776f0b61f56a9914cd93eeef8ed1f
Jade Meskill <jade.meskill@gmail.com>
2017-03-02 14:29:52 -0700

    use latest octoswarm

--- Formula/octoswarm.rb
+++ Formula/octoswarm.rb
@@ -1,9 +1,9 @@
 class Octoswarm < Formula
-  VERSION = "9.1.2"
+  VERSION = "9.2.0"
 
   desc "Unix dev tools created by Octoblu"
   url "https://github.com/octoblu/ops-tools-octoswarm/archive/v#{VERSION}.tar.gz"
-  sha256 "990eec7fd83bc3ca0ccc08f754ef949945bda033b0bda6c9ad3a039b5c1e16a8"
+  sha256 "28422ac5be203ef76d5be337ee37ee3981179d64fc128c6a4d8338a8a584b397"
   depends_on 'jq'
 
   def install
