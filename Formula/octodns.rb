f3d5a9ba23db8e3f45f16e707f3f3803775f7634
Roy van de Water <r@octoblu.com>
2017-05-30 09:55:40 -0700

    upgrade to octodns v2.0.4

--- Formula/octodns.rb
+++ Formula/octodns.rb
@@ -1,20 +1,29 @@
 class Octodns < Formula
-  VERSION = "1.0.1"
+  VERSION = "2.0.4"
+  BIN_FILES = ["octodns", "octodns-alias", "octodns-srv"]
 
   desc "Update Octoblu DNS records"
   url "https://github.com/octoblu/ops-tools-octodns/archive/v#{VERSION}.tar.gz"
-  sha256 "08dc291c8c30de2df4f44454cece3279777f6505c05150699748e702d54bcbb8"
+  sha256 "5d26e790ae80d33dcb3dee65a753f63243b069bae697c24b90f85b9d4a189308"
   depends_on 'jq'
 
   def install
-    inreplace "octodns.sh", 'directory="$(script_directory)"', "echo \"#{VERSION}\" && return 0"
+    # Replace VERSION
+    inreplace "octodns", '# HOMEBREW HOOK: early return version', "echo \"#{VERSION}\" && return 0"
+    inreplace "octodns-alias", '# HOMEBREW HOOK: early return version', "echo \"#{VERSION}\" && return 0"
+    inreplace "octodns-srv", '# HOMEBREW HOOK: early return version', "echo \"#{VERSION}\" && return 0"
 
-    move "octodns.sh", "octodns"
+    # Replace subcommand paths
+    inreplace "octodns", '# HOMEBREW HOOK: early return action', "echo -n \"#{prefix}/octodns-$action\" && return 0"
 
     bin.install "octodns"
+    prefix.install "octodns-alias"
+    prefix.install "octodns-srv"
   end
 
   test do
     assert_equal "#{VERSION}", shell_output("#{bin}/octodns --version").strip
+    assert_equal "#{VERSION}", shell_output("#{bin}/octodns alias --version").strip
+    assert_equal "#{VERSION}", shell_output("#{bin}/octodns srv --version").strip
   end
 end
