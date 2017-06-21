6133c1f714e7c6d3a763102257213f3d53c34b3d
Roy van de Water <r@octoblu.com>
2017-06-21 13:41:43 -0700

    upgrade bashblu

--- Formula/bashblu.rb
+++ Formula/bashblu.rb
@@ -1,16 +1,18 @@
 class Bashblu < Formula
-  VERSION = "3.0.1"
+  VERSION = "3.0.3"
 
   desc "Generate bash scripts"
   url "https://github.com/octoblu/unix-dev-tools-bashblu/archive/v#{VERSION}.tar.gz"
-  sha256 "9d66075f709dfd7b89d515703d5675c1562fe32804b12f34c305f4953eb78153"
+  sha256 "5136b4f6bf02702f8208ae544026906d7c419c5759f3aefedd9be50afc840f86"
 
   def install
-    inreplace "bashblu.sh", 'local directory="$(script_directory)"', ""
-    inreplace "bashblu.sh", 'echo "unknown-version"', "echo '#{VERSION}'"
+    inreplace "bashblu.sh", '# HOMEBREW HOOK: version', "echo \"#{VERSION}\" && return 0"
+    inreplace "bashblu.sh", '# HOMEBREW HOOK: get_template', "cat \"#{prefix}/basic-script.sh\" && return 0"
 
     move "bashblu.sh", "bashblu"
 
+    prefix.install "VERSION"
+    prefix.install "templates/basic-script.sh"
     bin.install "bashblu"
   end
 
